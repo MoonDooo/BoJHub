e2a1035b6b3792072124063c27b14b441016ed46
MoonDooo <47065431+MoonDooo@users.noreply.github.com>
2024-08-11 18:09:50 +0900

    [Bronze V] Title: A/B, Time: 76 ms, Memory: 11612 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        new sol();
+    }
+}
+class sol{
+    Buf buf = new Buf();
+    sol() throws IOException {
+        int a = buf.getInt();
+        int b = buf.getInt();
+        System.out.println((double)a/b);
+    }
+}
+class Buf{
+    BufferedReader br;
+    StringTokenizer st;
+    Buf() {
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+    public String get(){
+        while(st==null||!st.hasMoreElements()){
+            try{
+                st= new StringTokenizer(br.readLine());
+            }catch(IOException e){
+                return null;
+            }
+        }
+        return st.nextToken();
+    }
+    public Integer getInt(){
+        return Integer.parseInt(get());
+    }
+}

09b24ca43ddeffc42fb9f5084cf822d4e61c97df
MoonDooo <47065431+MoonDooo@users.noreply.github.com>
2024-08-11 18:19:48 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 88 ms, Memory: 11720 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        new sol();
+    }
+}
+class sol{
+    sol() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuffer sb = new StringBuffer();
+        for (int i =0 ; i<n; i++){
+            for (int j  =0; j<n; j++){
+                if (j < n-i-1)sb.append(" ");
+                else sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
